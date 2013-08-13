6b987e517472b78163f76465b91278f5f76ba898
biosed <stephen.meehan@gmail.com>
2013-08-14 00:01:55 +0100

    updated

--- agent.go
+++ agent.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	// Listen on TCP port 2000 on all interfaces.
+	l, err := net.Listen("tcp", ":2000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+	fmt.Printf("SCM Agent started...\n")
+	for {
+		// Wait for a connection.
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Handle the connection in a new goroutine.
+		// The loop then returns to accepting, so that
+		// multiple connections may be served concurrently.
+		go func(c net.Conn) {
+			// Echo all incoming data.
+			io.WriteString(c, "Hello")
+			// Shut down the connection.
+			c.Close()
+		}(conn)
+	}
 }
